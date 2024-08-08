52197b96f1b52367c22c472c2ed83dfd5b5ee36a
Eduardo Henao <eduardo.henao@outlook.com>
2024-08-08 11:03:50 -0500

    refacto and added delete functions

--- main.go
+++ main.go
@@ -11,15 +11,37 @@ import (
 )
 
 func main() {
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion("us-east-1"),
-	)
+	bucketName := "hello-world-789"
+	s3svc := InitAws()
+
+	BucketsList(s3svc)
+
+	BucketCreate(s3svc, bucketName)
+	BucketsList(s3svc)
+
+	BucketDelete(s3svc, bucketName)
+	BucketsList(s3svc)
+}
+
+func BucketDelete(s3svc *s3.Client, bucketName string) {
+	_, err := s3svc.DeleteBucket(context.TODO(), &s3.DeleteBucketInput{
+		Bucket: aws.String(bucketName),
+	})
 	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
+		log.Fatalf("failed to delete bucket %v", err)
 	}
+}
 
-	s3svc := s3.NewFromConfig(cfg)
+func BucketCreate(s3svc *s3.Client, bucketName string) {
+	_, err := s3svc.CreateBucket(context.TODO(), &s3.CreateBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		log.Fatalf("failed to create bucket %v", err)
+	}
+}
 
+func BucketsList(s3svc *s3.Client) {
 	resp, err := s3svc.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
 	if err != nil {
 		log.Fatalf("failed to list buckets, %v", err)
@@ -33,4 +55,15 @@ func main() {
 	} else {
 		fmt.Println("no buckets found")
 	}
+}
+
+func InitAws() *s3.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	return s3.NewFromConfig(cfg)
 }
